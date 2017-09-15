9ac1b86e3dde42c527ce6d8bd46b0a94d3401753
Vasiliy <devasidmi@gmail.com>
2017-09-15 19:14:39 +0300

    added spa

--- frontend/app.js
+++ frontend/app.js
@@ -11,20 +11,13 @@ var ejs = require('ejs');
 var url = require('url');
 
 ejs.delimiter = '?';
+
+app.set('view engine', 'html');
 app.set('views', path.join(__dirname, '/public/templates'));
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
 
 app.get('/', function (req, res) {
     res.render('index');
-});
-
-app.get('/registration', function (req, res) {
-    res.render('registration');
-});
-
-app.get('/about', function (req, res) {
-    res.render('about');
 });
 
 app.get('*', function (req, res) {
