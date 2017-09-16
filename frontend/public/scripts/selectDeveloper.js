916fd5a7bb2b1c043c8bd2bd4964e123b13cfb31
ChocolateSwan <olyasur1996@gmale.com>
2017-09-16 10:06:51 +0300

    validation

--- frontend/public/scripts/selectDeveloper.js
+++ frontend/public/scripts/selectDeveloper.js
@@ -1,33 +1,27 @@
 $(".ui.circular.image").click(function(){
-
-
-    var title = document.getElementById("title");
-    var avatar = document.getElementById("developer-avatar");
-    var workOn = document.getElementById("workOn");
-
     switch(this.id){
         case "1":
-        title.innerText = "Ольга Сурикова"
-        avatar.src = "/images/olga_surikova.jpg"
-        workOn.innerText = "Frontend"
+        $("div[class='header']").text("Ольга Сурикова")
+        $("div[class='ui medium image'] img").attr("src","/images/olga_surikova.jpg")
+        $("div[class='ui header']").text("Frontend")
         break;
         
         case "2":
-        title.innerText = "Василий Дмитриев"
-        avatar.src = "/images/vasiliy_dmitriev.jpg"
-        workOn.innerText = "Frontend"
+        $("div[class='header']").text("Василий Дмитриев")
+        $("div[class='ui medium image'] img").attr("src","/images/vasiliy_dmitriev.jpg")
+        $("div[class='ui header']").text("Frontend")
         break;
 
         case "3":
-        title.innerText = "Никита Боярских"
-        avatar.src = "/images/nikita_boyarskikh.jpg"
-        workOn.innerText = "Backend"
+        $("div[class='header']").text("Никита Боярских")
+        $("div[class='ui medium image'] img").attr("src","/images/nikita_boyarskikh.jpg")
+        $("div[class='ui header']").text("Backend")
         break;
 
         case "4":
-        title.innerText = "Егор Кураков"
-        avatar.src = "/images/egor_kurakov.jpg"
-        workOn.innerText = "Backend"
+        $("div[class='header']").text("Егор")
+        $("div[class='ui medium image'] img").attr("src","/images/none.png")
+        $("div[class='ui header']").text("Backend")
         break;
     }
     $('.ui.modal')
