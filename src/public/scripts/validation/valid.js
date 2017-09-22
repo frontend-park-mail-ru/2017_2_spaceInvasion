9dff8f185fd4df2fdcf227eedeb87ce1501b3cd8
ChocolateSwan <olyasur1996@gmale.com>
2017-09-22 15:00:21 +0300

    fix validation

--- src/public/scripts/validation/valid.js
+++ src/public/scripts/validation/valid.js
@@ -1,36 +1,48 @@
 'use strict';
 
-// TODO: Дописать переменные email, login,...
+// Удаление начальных и конечных пробелов.
+const deleteStartEndSpacesFromStr = (str) =>
+    str.replace(/^\s*/, '').replace(/\s*$/, '');
 
+// Удалить первые и последние пробелы из инпутов.
+const deleteStartEndSpaces = function () {
+    for (let element of loginForm.elements)
+        element.value = deleteStartEndSpacesFromStr(element.value);
+};
+
+// Получение данных из формы.
 const loginForm = document.querySelector('.formWithValidation');
+deleteStartEndSpaces();
 const validateBtn = loginForm.querySelector('.validateBtn');
 const password = loginForm.password;
 const repeatedPassword = loginForm.repeatedPassword;
-let isError = false;  // TODO: Надо убрать!!!!
+const login = loginForm.login;
+const email = loginForm.email;
+const conditions = loginForm.conditions;
 
 //=================================================================
 
 // Критерии проверки валидности.
 const rulesForEmail = [{
-    predicate: email => /^[\w\-]+\@[\w\-]+\.[A-Za-z]{2,4}$/.test(email),//
+    predicate: email => /^[\w\-]+@[\w\-]+\.[A-Za-z]{2,4}$/.test(email),//
     message: "Wrong format of email",
-},{ predicate: function (email) { return email !== ""; },
+},{ predicate: email => email !== "",
     message: "Email can not be blank",
 }];
 const rulesForConditions = [{
-    predicate: function (isChecked) {return isChecked;},
+    predicate: isChecked => isChecked,
     message: "Agree to the Terms and Conditions, please",
 }];
 const rulesForLogin = [{
-    predicate: function (login) {return /^[\w\-\@]{5,}$/.test(login)},
+    predicate: login => /^[\w\-@]{5,}$/.test(login),
     message: "Minimum login length is 5",
-},{ predicate: function (login) { return /^[\w\-\@]+$/.test(login) },
+},{ predicate: login => /^[\w\-@]+$/.test(login),
     message: "Login must contain only latin symbols,digits,_,-,@",
 }];
 const rulesForPassword = [{
-    predicate: function (password) {return /^[\w\s\-\.]{8,}$/.test(password)},
+    predicate: password => /^[\w\s\-.]{8,}$/.test(password),
     message: "Minimum password length is 8",
-},{ predicate: function (password) {return /^[\w\s\-\.]+$/.test(password)},
+},{ predicate: password => /^[\w\s\-.]+$/.test(password),
     message: "Passwords must contain only latin symbols,digits,_,-,.",
 }];
 
@@ -68,20 +80,17 @@ function auth(login, email, password, callback) {
 const validate = (target, rules) =>
     rules.filter(rule => !rule.predicate(target)).map(rule => rule.message);
 
-// Удаление начальных и конечных пробелов.
-const deleteStartEndSpaces = (str) =>
-    str.replace(/^\s*/, '').replace(/\s*$/, '');
-
 // Вставить элемент после заданного.
 const insertAfter = (parent, node, referenceNode) =>
     parent.insertBefore(node, referenceNode.nextSibling);
 
-// Создание ошибки (сообщения и красной рамки)
-const generateError = function (element, arrayOfErrors) {
+// Создание ошибки (сообщения и красной рамки).
+const generateError = function (arrayOfErrors,...element) {
     if (arrayOfErrors.length !== 0){
-        generateRedBorder(element);
+        element.forEach(i => generateRedBorder(i));
         generateErrorMessage(arrayOfErrors);
     }
+    return arrayOfErrors.length;
 };
 
 // Создание красной рамки у ошибочного поля.
@@ -90,66 +99,65 @@ const generateRedBorder = (element) =>
 
 //Создание сообщения об ошибке.
 const generateErrorMessage = function (arrayOfErrors) {
-    for (let i in arrayOfErrors) {
-        isError = true; // TODO: Надо убрать!!!!
+    for (let i of arrayOfErrors) {
         const error = document.createElement('div');
         error.className = 'error';
         error.style.color = 'red';
-        error.innerHTML = arrayOfErrors[i];
+        error.innerHTML = i;
         insertAfter(validateBtn.parentElement, error, validateBtn);
     }
 };
 
 //Удаление красных рамок.
-const removeRedBorders = function (){  // TODO: Переписать!!!!
-loginForm.email.style.borderColor = "";
-loginForm.login.style.borderColor ="";
-loginForm.password.style.borderColor ="";
-loginForm.repeatedPassword.style.borderColor ="";
+const removeRedBorders = function (){
+    for (let element of loginForm.elements)
+        element.style.borderColor = "";
+};
+
+//Удаление всех сообщений об ошибках.
+const removeErrorMessages = function () {
+    const errors = loginForm.querySelectorAll('.error');
+    for (let i = 0; i < errors.length; i++) {
+        errors[i].remove();
+    }
 };
 
 // Удаление ненужных ошибок.
 const removeErrors = function () {
     removeRedBorders();
-    // TODO: Нет удаления красных рамок!
-        const errors = loginForm.querySelectorAll('.error');
-        for (let i = 0; i < errors.length; i++) {
-            isError = false; // TODO: Надо убрать!!!
-            errors[i].remove();
-        }
+    removeErrorMessages();
+};
+
+//Обработка данных формы.
+const processFormData = function () {
+
+    let errorSum = 0;
+
+    // Проверки валидности.
+    errorSum += generateError(validate(conditions.checked, rulesForConditions), loginForm.conditions);
+    if (password.value !== repeatedPassword.value){
+        generateError(['Password doesn\'t match'],password,repeatedPassword);
+    } else {
+        errorSum += generateError(validate(password.value, rulesForPassword),password, repeatedPassword);
+    }
+    errorSum += generateError(validate(login.value,rulesForLogin), login);
+    errorSum += generateError(validate(email.value, rulesForEmail), email);
+
+    return errorSum;
 };
 
 // Обработчик формы.
 loginForm.addEventListener('submit', function (event) {
 
-    debugger
     event.preventDefault();
     removeErrors();
 
-    // Проверки валидности.
-    generateError(loginForm.conditions, validate(loginForm.conditions.checked, rulesForConditions));
-// TODO: По другому сделать проверку пароля!!!
-    loginForm.password = deleteStartEndSpaces(loginForm.password.value);
-    loginForm.repeatedPassword = deleteStartEndSpaces(loginForm.repeatedPassword.value);
-    if (password.value !== repeatedPassword.value){ 
-        generateErrorMessage(['Password doesn\'t match']);
-        generateRedBorder(loginForm.repeatedPassword);
-        generateRedBorder(loginForm.password);
-    } else {
-        generateError(password,validate(password.value, rulesForPassword));
-        //generateRedBorder(loginForm.repeatedPassword);
-    }
-    generateError(loginForm.login, validate(loginForm.login.value,rulesForLogin));
-    generateError(loginForm.email, validate(loginForm.email.value, rulesForEmail));
-
-    if (!isError) { 
-        // Авторизация.
-        auth(loginForm.login.value, loginForm.email.value, loginForm.password.value, function (err, resp) {
+    if (!processFormData()) {
+        auth(login.value, email.value, password.value, function (err, resp) {
                 if (err) {
                     return alert('AUTH Error: ${err.status}.Please, send data again.');
                 }
                 console.log("Authorization OK!")
                 //loginForm.reset();
             });}
-
 });
