7ebed38b0d7bb6e5eb6d320b352c79a4c4658d75
Vasiliy <devasidmi@gmail.com>
2017-09-28 23:26:03 +0300

    - leaderboard bug fixes

--- public/blocks/leaderboard/index.js
+++ public/blocks/leaderboard/index.js
@@ -14,12 +14,14 @@
 
     function fetchLeaderboard() {
 
-        const res = Http.FetchGet("/leaderboard");
+        Http.FetchGet("/leaderboard").then(function(res) {
+            return res;
+        });
 
         // const jsonData = '[{ "username": "vasidmi", "email": "https://t.me/vasidmi", "score": 0 }, { "username": "boyanik", "email": "https://t.me/Nikita_Boyarskikh", "score": 0 }, { "username": "egor", "email": "https://Egor_Kurakov", "score": 0 }, { "username": "ChocolateSwan", "email": "https://t.me/ChocolateSwan", "score": 0 }]';
-        const leaderBoardObject = JSON.parse(res);
+        // const leaderBoardObject = JSON.parse(res);
 
-        return leaderBoardObject;
+        // return leaderBoardObject;
     }
 
     window.Leaderboard = Leaderboard;
