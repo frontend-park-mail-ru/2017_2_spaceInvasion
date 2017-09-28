964a868161aef367f154888659a3dae9afb700fe
Vasiliy <devasidmi@gmail.com>
2017-09-28 22:59:31 +0300

    - Now getting leaderboard from api!

--- public/blocks/leaderboard/index.js
+++ public/blocks/leaderboard/index.js
@@ -16,8 +16,8 @@
 
         const res = Http.FetchGet("/leaderboard");
 
-        const jsonData = '[{ "username": "vasidmi", "email": "https://t.me/vasidmi", "score": 0 }, { "username": "boyanik", "email": "https://t.me/Nikita_Boyarskikh", "score": 0 }, { "username": "egor", "email": "https://Egor_Kurakov", "score": 0 }, { "username": "ChocolateSwan", "email": "https://t.me/ChocolateSwan", "score": 0 }]';
-        const leaderBoardObject = JSON.parse(jsonData);
+        // const jsonData = '[{ "username": "vasidmi", "email": "https://t.me/vasidmi", "score": 0 }, { "username": "boyanik", "email": "https://t.me/Nikita_Boyarskikh", "score": 0 }, { "username": "egor", "email": "https://Egor_Kurakov", "score": 0 }, { "username": "ChocolateSwan", "email": "https://t.me/ChocolateSwan", "score": 0 }]';
+        const leaderBoardObject = JSON.parse(res);
 
         return leaderBoardObject;
     }
