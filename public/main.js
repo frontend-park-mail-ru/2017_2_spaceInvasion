bad1cefbcb7944501fcfbc1c4b66047e8901c67b
Vasiliy <devasidmi@gmail.com>
2017-09-30 00:15:14 +0300

    - quick fix

--- public/main.js
+++ public/main.js
@@ -1,14 +1,6 @@
 (function() {
     'use strict';
 
-    const Http = window.Http;
-
-    if (window.location.host === 'space-invasion.herokuapp.com') {
-        // enable CORS
-        Http.BaseUrl = 'https://space-invasion-backend.herokuapp.com';
-    }
-
-
     const Block = window.Block;
     const Login = window.Login;
     const About = window.About;
