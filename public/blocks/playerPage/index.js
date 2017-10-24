dd65421c465badef52d563d1fa7f4b7e1a519371
Vasiliy <devasidmi@gmail.com>
2017-10-24 15:51:23 +0300

    Hotfix (x2) router

--- public/blocks/playerPage/index.js
+++ public/blocks/playerPage/index.js
@@ -2,22 +2,24 @@ import Block from '../block/index';
 import userService from '../../services/userService';
 import { showHome } from '../../main';
 import playerpageTemplate from './playerPage.pug';
+import { router } from '../../main';
 
 function onLogoutBtnClick(el) {
-  el.querySelector('.ui.logout').addEventListener('click', () => {
-    userService.logout().then(() => {
-      showHome();
+    el.querySelector('.ui.logout').addEventListener('click', () => {
+        userService.logout().then(() => {
+            router.setPath('/');
+            showHome();
+        });
     });
-  });
 }
 
 class PlayerPage extends Block {
-  constructor() {
-    const el = document.createElement('div');
-    el.innerHTML = playerpageTemplate({ user: userService.user });
-    onLogoutBtnClick(el);
-    super(el);
-  }
+    constructor() {
+        const el = document.createElement('div');
+        el.innerHTML = playerpageTemplate({ user: userService.user });
+        onLogoutBtnClick(el);
+        super(el);
+    }
 }
 
 export default PlayerPage;
--- public/services/userService.js
+++ public/services/userService.js
@@ -1,52 +1,55 @@
 import Http from '../modules/httpModule';
-import { showPlayerPage, showHome, showError } from '../main';
+import { showPlayerPage, showHome, showError, router } from '../main';
 
 class UserService {
-  constructor() {
-    this.user = null;
-    this.users = [];
-  }
-
-  logout() {
-    const result = Http.Fetch('POST', '/user/logout');
-    if (result) {
-      this.user = null;
+    constructor() {
+        this.user = null;
+        this.users = [];
     }
-    return result;
-  }
 
-  // Регистрация пользователя
-  register(email, username, password) {
-    this.user = Http.Fetch('POST', '/user/signup', { email, username, password });
-    if (this.user && !this.users.find(el => el.username === this.user.username)) {
-      this.users.push(this.user);
+    logout() {
+        const result = Http.Fetch('POST', '/user/logout');
+        if (result) {
+            this.user = null;
+        }
+        return result;
     }
-    return this.user;
-  }
 
-  // Авторизация пользователя
-  login(username, password) {
-    this.user = Http.Fetch('POST', '/user/signin', { username, password });
-    return this.user;
-  }
+    // Регистрация пользователя
+    register(email, username, password) {
+        this.user = Http.Fetch('POST', '/user/signup', { email, username, password });
+        if (this.user && !this.users.find(el => el.username === this.user.username)) {
+            this.users.push(this.user);
+        }
+        return this.user;
+    }
 
-  // Залогинен ли пользователь
-  isLoggedIn() {
-    return Boolean(this.user);
-  }
+    // Авторизация пользователя
+    login(username, password) {
+        this.user = Http.Fetch('POST', '/user/signin', { username, password });
+        return this.user;
+    }
 
-  getData() {
-    Http.Fetch('GET', '/user').then(userdata => userdata.json())
-      .then((userdata) => {
-        const path = window.location.pathname;
-        if (userdata.result !== 'Unauthorized') {
-          this.user = userdata;
-          if (path === '/login' || path === '/profile' || path === '/') { showPlayerPage(); }
-        } else if (path === '/login' || path === '/profile') { showHome(); }
-      }).catch(() => {
-        showError('Oops, try again!');
-      });
-  }
+    // Залогинен ли пользователь
+    isLoggedIn() {
+        return Boolean(this.user);
+    }
+
+    getData() {
+        Http.Fetch('GET', '/user').then(userdata => userdata.json())
+            .then((userdata) => {
+                const path = window.location.pathname;
+                if (userdata.result !== 'Unauthorized') {
+                    this.user = userdata;
+                    if (path === '/login' || path === '/profile' || path === '/') { showPlayerPage();
+                        router.setPath('/profile'); }
+                } else if (path === '/login' || path === '/profile') { showHome();
+                    router.setPath('/'); }
+            }).catch(() => {
+                router.setPath('/');
+                showError('Oops, try again!');
+            });
+    }
 }
 
 const userService = new UserService();
