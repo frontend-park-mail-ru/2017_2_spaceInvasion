9d971035895c1eb6ea2b81b3b5686e4d8d49770f
Nikita-Boyarskikh <N02@yandex.ru>
2017-10-25 00:19:40 +0300

    Configure TypeScript with webpack (light checkstyle)

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
 const debug = process.env.NODE_ENV !== 'production';
-require('webpack');
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
   context: path.join(__dirname, '/public'),
   devtool: debug ? 'source-map' : null,
-  entry: './index.js',
+  plugins: debug ? [] : [
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
+  entry: './index.ts',
   output: {
     path: path.join(__dirname, '/public/dist'),
     filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.css', '.pug', '.ts', '.tsx', '.js', '.jsx'],
   },
   module: {
     loaders: [
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       { test: /\.pug$/, use: ['pug-loader'] },
+      { test: /\.tsx?$/, use: ['ts-loader'], exclude: /node_modules/ },
     ],
   },
 };
