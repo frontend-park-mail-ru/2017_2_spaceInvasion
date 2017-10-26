3665a10c2632b80fe2f5cefd8bd1b308207a6d6b
Vasiliy <devasidmi@gmail.com>
2017-10-26 03:53:38 +0300

    Fix lint warnings. Added JQery in Lint config

--- public/main.js
+++ public/main.js
@@ -15,177 +15,177 @@ const PNotify = require('./pnotify.custom.min.js');
 window.router = router;
 
 const sections = {
-    login: Block.Create('section', {}, ['login-section']),
-    signup: Block.Create('section', {}, ['signup-section']),
-    about: Block.Create('section', {}, ['about-section']),
-    leaderboard: Block.Create('section', {}, ['leaderboard-section']),
-    playerpage: Block.Create('section', {}, ['playerpage-section']),
+  login: Block.Create('section', {}, ['login-section']),
+  signup: Block.Create('section', {}, ['signup-section']),
+  about: Block.Create('section', {}, ['about-section']),
+  leaderboard: Block.Create('section', {}, ['leaderboard-section']),
+  playerpage: Block.Create('section', {}, ['playerpage-section']),
 
-    hide() {
-        this
-            .login
-            .hide();
-        this
-            .signup
-            .hide();
-        this
-            .about
-            .hide();
-        this
-            .leaderboard
-            .hide();
-        this
-            .playerpage
-            .hide();
-    },
+  hide() {
+    this
+      .login
+      .hide();
+    this
+      .signup
+      .hide();
+    this
+      .about
+      .hide();
+    this
+      .leaderboard
+      .hide();
+    this
+      .playerpage
+      .hide();
+  },
 };
 
 sections.hide();
 
 app
-    .append(sections.login)
-    .append(sections.signup)
-    .append(sections.about)
-    .append(sections.leaderboard)
-    .append(sections.playerpage);
+  .append(sections.login)
+  .append(sections.signup)
+  .append(sections.about)
+  .append(sections.leaderboard)
+  .append(sections.playerpage);
 
 function dismissAllMessages() {
-    PNotify.removeAll();
+  PNotify.removeAll();
 }
 
 userService.getData();
 router.start();
 
 function showError(message) {
-    return new PNotify({
-        title: 'Error',
-        text: message,
-        type: 'error',
-    });
+  return new PNotify({
+    title: 'Error',
+    text: message,
+    type: 'error',
+  });
 }
 
 function hideSignUp() {
-    signUpBtn.setAttribute('style', 'display:none');
+  signUpBtn.setAttribute('style', 'display:none');
 }
 
 function openPlayerPage() {
-    dismissAllMessages();
-    signUpBtn.setAttribute('style', 'display:none');
-    sections.hide();
-    if (!sections.playerpage.ready) {
-        sections.playerpage.append(new PlayerPage());
-        sections.playerpage.ready = true;
-    } else {
-        sections.playerpage.el.removeChild(sections.playerpage.el.firstChild);
-        sections.playerpage.append(new PlayerPage());
-    }
-    router.setPath('/profile');
-    sections.playerpage.show();
+  dismissAllMessages();
+  signUpBtn.setAttribute('style', 'display:none');
+  sections.hide();
+  if (!sections.playerpage.ready) {
+    sections.playerpage.append(new PlayerPage());
+    sections.playerpage.ready = true;
+  } else {
+    sections.playerpage.el.removeChild(sections.playerpage.el.firstChild);
+    sections.playerpage.append(new PlayerPage());
+  }
+  router.setPath('/profile');
+  sections.playerpage.show();
 }
 
 // Отправка формы логина.
 function onSubmitLoginForm(formdata) {
-    const loginBtn = document.querySelector('.ui.submit.button');
-    loginBtn.classList.add('loading');
-    userService.login(formdata.login, formdata.password).then(data => data.json())
-        .then((data) => {
-            if (data.result !== 'Singning in failed') {
-                userService.user = data;
-                loginBtn.classList.remove('loading');
-                sections.login.loginform.reset();
-                hideSignUp();
-                openPlayerPage();
-            } else {
-                loginBtn.classList.remove('loading');
-                showError(data.result);
-            }
-        });
+  const loginBtn = document.querySelector('.ui.submit.button');
+  loginBtn.classList.add('loading');
+  userService.login(formdata.login, formdata.password).then(data => data.json())
+    .then((data) => {
+      if (data.result !== 'Singning in failed') {
+        userService.user = data;
+        loginBtn.classList.remove('loading');
+        sections.login.loginform.reset();
+        hideSignUp();
+        openPlayerPage();
+      } else {
+        loginBtn.classList.remove('loading');
+        showError(data.result);
+      }
+    });
 }
 
 // Отправка формы регистрации.
 function onSubmitRegistrationForm(formdata) {
-    const regBtn = document.querySelector('.validateBtn');
-    regBtn.classList.add('loading');
-    userService.register(formdata.email, formdata.login, formdata.password).then(data => data.json())
-        .then((data) => {
-            if (data.result !== 'Username already used') {
-                userService.user = data;
-                regBtn.classList.remove('loading');
-                sections.signup.signupform.reset();
-                hideSignUp();
-                openPlayerPage();
-            } else {
-                regBtn.classList.remove('loading');
-                showError(data.result);
-            }
-        });
+  const regBtn = document.querySelector('.validateBtn');
+  regBtn.classList.add('loading');
+  userService.register(formdata.email, formdata.login, formdata.password).then(data => data.json())
+    .then((data) => {
+      if (data.result !== 'Username already used') {
+        userService.user = data;
+        regBtn.classList.remove('loading');
+        sections.signup.signupform.reset();
+        hideSignUp();
+        openPlayerPage();
+      } else {
+        regBtn.classList.remove('loading');
+        showError(data.result);
+      }
+    });
 }
 
 function openLogin() {
-    if (!userService.isLoggedIn()) {
-        signUpBtn.setAttribute('style', '');
-        sections.hide();
-        if (!sections.login.ready) {
-            sections.login.loginform = new Login();
-            sections.login.append(sections.login.loginform);
-            sections.login.loginform.onSubmit(onSubmitLoginForm);
-            sections.login.ready = true;
-        }
-        sections.login.loginform.reset();
-        router.setPath("/");
-        sections.login.show();
-    } else if (sections.playerpage.isHidden()) {
-        openPlayerPage();
+  if (!userService.isLoggedIn()) {
+    signUpBtn.setAttribute('style', '');
+    sections.hide();
+    if (!sections.login.ready) {
+      sections.login.loginform = new Login();
+      sections.login.append(sections.login.loginform);
+      sections.login.loginform.onSubmit(onSubmitLoginForm);
+      sections.login.ready = true;
     }
+    sections.login.loginform.reset();
+    router.setPath('/');
+    sections.login.show();
+  } else if (sections.playerpage.isHidden()) {
+    openPlayerPage();
+  }
 }
 
 function openRegistration() {
-    dismissAllMessages();
-    sections.hide();
-    if (!sections.signup.ready) {
-        sections.signup.signupform = new Registration();
-        sections.signup.append(sections.signup.signupform);
-        sections.signup.signupform.onSubmit(onSubmitRegistrationForm);
-        sections.signup.ready = true;
-    }
-    sections.signup.signupform.reset();
-    router.setPath('/signup');
-    sections.signup.show();
+  dismissAllMessages();
+  sections.hide();
+  if (!sections.signup.ready) {
+    sections.signup.signupform = new Registration();
+    sections.signup.append(sections.signup.signupform);
+    sections.signup.signupform.onSubmit(onSubmitRegistrationForm);
+    sections.signup.ready = true;
+  }
+  sections.signup.signupform.reset();
+  router.setPath('/signup');
+  sections.signup.show();
 }
 
 function openLeaderboard() {
-    dismissAllMessages();
-    sections.hide();
-    if (!sections.leaderboard.ready) {
-        sections
-            .leaderboard
-            .append(new Leaderboard());
-        sections.leaderboard.ready = true;
-    }
-    router.setPath('/leaderboard');
+  dismissAllMessages();
+  sections.hide();
+  if (!sections.leaderboard.ready) {
     sections
-        .leaderboard
-        .show();
+      .leaderboard
+      .append(new Leaderboard());
+    sections.leaderboard.ready = true;
+  }
+  router.setPath('/leaderboard');
+  sections
+    .leaderboard
+    .show();
 }
 
 function openAbout() {
-    dismissAllMessages();
-    sections.hide();
-    if (!sections.about.ready) {
-        sections.about.append(new About());
-        sections.about.ready = true;
-        alertDialog();
-    }
-    router.setPath('/about');
-    sections.about.show();
+  dismissAllMessages();
+  sections.hide();
+  if (!sections.about.ready) {
+    sections.about.append(new About());
+    sections.about.ready = true;
+    alertDialog();
+  }
+  router.setPath('/about');
+  sections.about.show();
 }
 
 export {
-    openLogin as showHome,
-    openAbout as showAbout,
-    openRegistration as showRegistration,
-    openLeaderboard as showLeaderboard,
-    openPlayerPage as showPlayerPage,
-    showError,
-    router,
+  openLogin as showHome,
+  openAbout as showAbout,
+  openRegistration as showRegistration,
+  openLeaderboard as showLeaderboard,
+  openPlayerPage as showPlayerPage,
+  showError,
+  router,
 };
