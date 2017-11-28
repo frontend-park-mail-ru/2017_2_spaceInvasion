326c101909e3ce921b7ce1141e58c383c53f92a0
Nikita-Boyarskikh <N02@yandex.ru>
2017-11-29 01:28:46 +0300

    Mock PNotify to not use JQuery
    TODO: Create PNotify ourself

---
+++ public/utils/notifications.ts
@@ -0,0 +1,50 @@
+import Navigator from '../modules/navigator';
+
+class PNotify {
+  constructor(data: any) {
+    console.log(data);
+  }
+
+  static removeAll() {
+    console.log('remove it');
+  }
+}
+
+function dismissAllMessages(): void {
+  PNotify.removeAll();
+}
+
+function showError(message: string): void {
+  new PNotify({
+    buttons: {
+      sticker: false
+    },
+    title: 'Error',
+    text: message,
+    type: 'error',
+  });
+}
+
+function showLeaveGameNotification() {
+  const backToGameID = 'backToGame';
+  new PNotify({
+    title: 'Игра идёт',
+    type: 'info',
+    text: '<a id=' + backToGameID + '>Вернитесь в игру!</a>',
+    nonblock: {
+      nonblock: true,
+    },
+    animation: 'none',  // NOTE: Bug in PNotify (https://github.com/sciactive/pnotify/issues/241#issuecomment-340626073)
+    hide: false,
+    after_open: () => {
+      const link = document.getElementById(backToGameID);
+      if (link !== null) {
+        link.addEventListener('click', Navigator.sections.game.show);
+      }
+      return true;
+    }
+  });
+}
+
+export default PNotify;
+export {dismissAllMessages, showError, showLeaveGameNotification};
