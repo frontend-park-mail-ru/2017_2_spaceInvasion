a307e8a1ece798052bfc9addbaf26255b7d6cb3e
Nikita-Boyarskikh <N02@yandex.ru>
2017-11-29 01:57:08 +0300

    Revrite gameService from js to ts and make it singleton

---
+++ public/services/gameService.ts
@@ -0,0 +1,127 @@
+import PNotify from '../utils/notifications';
+import User from '../models/user';
+import GameScene from '../models/game/gameScene';
+import {ConstructableController, default as ControllerInterface} from '../modules/game/controllers/controllerInterface';
+import {ConstructableStrategy, default as StrategyInterface} from '../modules/game/strateges/strategyInterface';
+import {SIDE} from '../utils/constants';
+import emitter from '../modules/emitter';
+import SubscriptableMixin from '../models/game/mixins/subscriptableMixin';
+import Navigator from '../modules/navigator';
+import LoginBlock from "../blocks/login/index";
+
+class GameService extends SubscriptableMixin {
+  private static instance = new GameService();
+  protected binded = false;
+  protected strategy: StrategyInterface;
+  protected scene: GameScene;
+  protected controllers: ControllerInterface;
+  protected running = false;
+  protected requestID: number;
+
+  constructor() {
+    super();
+
+    if (GameService.instance) {
+      return GameService.instance;
+    }
+    GameService.instance = this;
+  }
+
+  init(canvas: HTMLCanvasElement, strategy: ConstructableStrategy, controller: ConstructableController) {
+    if (this.isRunning()) {
+      this.destroy();
+    }
+
+    this.strategy = new strategy(this.onFinishGame.bind(this));
+    this.scene = new GameScene(canvas);
+    this.controllers = new controller;
+    this.running = true;
+
+    // Subscribes
+    this.subscribe('GameService.onFinishGame', this.onFinishGame); // victory : boolean
+  }
+
+  join(user: User, side: SIDE): void {
+    if (this.strategy.join(user, side)) {
+      this.bind();
+    } else {
+      // TODO: Подождите других игроков...
+    }
+  }
+
+  gameLoop(): void {
+    const controlsUpdates = Array.from(this.controllers.diff());
+
+    const newCommands = controlsUpdates.filter(el => el[1]).map(el => el[0]);
+    newCommands.forEach(cmd => {
+      try {
+        emitter.emit('Strategy.onNewCommand', cmd);
+      } catch (e) {
+        new PNotify({
+          title: 'Не получилось выполнить действие',
+          type: 'error',
+          text: e.message
+        });
+      }
+    });
+
+    const stopedCommands = controlsUpdates.filter(el => el[1]).map(el => el[0]);
+    stopedCommands.forEach(cmd => emitter.emit('Strategy.onStopCommand', cmd));
+
+    this.scene.render(this.strategy.getState());
+    this.requestID = requestAnimationFrame(this.gameLoop.bind(this));
+  }
+
+  onFinishGame(...data: any[]): void {
+    const victory = data[0] as boolean;
+    this.destroy();
+    Navigator.sections.hide();
+    (Navigator.sections.home as LoginBlock).show(); // TODO: Сверстать homepage
+    new PNotify({
+      title: 'Игра окончена',
+      type: (victory ? 'success' : 'notice'),
+      text: (victory ? 'Вы победили!' : 'Вы проиграли!'),
+      buttons: {
+        sticker: false,
+      },
+    });
+  }
+
+  isRunning(): boolean {
+    return this.running;
+  }
+
+  unbind(): void {
+    if (this.requestID) {
+      cancelAnimationFrame(this.requestID);
+    }
+
+    if (this.binded) {
+      this.scene.unbind();
+      this.controllers.destroy();
+      this.binded = false;
+    }
+  }
+
+  bind(): void {
+    this.requestID = requestAnimationFrame(this.gameLoop.bind(this));
+
+    if (!this.binded) {
+      this.scene.bind();
+      this.controllers.init();
+      this.binded = true;
+    }
+  }
+
+  destroy(): void {
+    this.running = false;
+    this.unbind();
+    this.strategy.destroy();
+    this.controllers.destroy();
+    this.scene.destroy();
+  }
+}
+
+const gameService = new GameService();
+export {GameService};
+export default gameService;
