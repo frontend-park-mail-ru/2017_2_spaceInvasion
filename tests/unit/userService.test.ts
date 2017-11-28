b3dfa4ff3b36a38451c220563d2e51af27582b87
Nikita-Boyarskikh <N02@yandex.ru>
2017-11-29 01:40:12 +0300

    Fix tests and add new ones: User and utils

--- tests/unit/userService.test.ts
+++ tests/unit/userService.test.ts
@@ -1,98 +1,111 @@
-// import Http from '../../public/modules/httpModule';
-// import userService from '../../public/services/userService';
-// import User from '../../public/models/user';
-//
-// const registredUsers = [];
-// let curUser = null;
-//
-// function refresh() {
-//   registredUsers.length = 0;
-//   curUser = null;
-// }
-//
-// function prepareFetchObject(object: User): User {
-//   if (!object || !object.username || !object.password) {
-//     return null;
-//   }
-//
-//   object.score = 0;
-//   delete object.password;
-//
-//   return object;
-// }
-//
-// // Mock Http.Fetch
-// Http.Fetch = (method = 'GET', path, object = {}): Promise<Response> => {
-//   switch (`${method} ${path}`) {
-//     case 'POST /user/signup':
-//       object = prepareFetchObject(object);
-//       if (curUser) {
-//         return Promise.resolve(null);
-//       }
-//       registredUsers.push(object);
-//       curUser = object;
-//       return Promise.resolve(curUser);
-//     case 'GET /user':
-//       return Promise.resolve(curUser);
-//     case 'GET /leaderboard':
-//       return Promise.resolve(registredUsers.sort((a, b) => a.score - b.score) as any);
-//     case 'POST /user/logout':
-//       if (curUser) {
-//         curUser = null;
-//         return Promise.resolve(true as any);
-//       }
-//       return Promise.resolve(false as any);
-//     case 'POST /user/signin':
-//       object = prepareFetchObject(object);
-//       if (curUser) {
-//         return Promise.resolve(null);
-//       }
-//       curUser = curUser || registredUsers.find(el => el.username === object.username &&
-//         el.password === object.password);
-//       return Promise.resolve(curUser);
-//     default:
-//       return Promise.reject(Error(`Origin API method ${method} ${path} calling is not permitted!`));
-//   }
-// };
-//
-// it('constructor', () => {
-//    expect(userService && !userService.user).toBeTruthy();
-//    expect(userService.users).toEqual([]);
-// });
-//
-// it('register', () => {
-//   refresh();
-//   const actual = { username: 'username', email: 'email', score: 0 };
-//   let expected;
-//   userService.register('email', 'username', 'password').then(data => expected = data);
-//   expect(expected).toBe(actual);
-//   expect(userService.users).toEqual([actual]);
-//   expect(userService.user).toEqual(actual);
-//   // expect(userService.fetch()).toBe(user);
-//   expect(userService.isLoggedIn()).toBeTruthy();
-// });
-//
-// it('login', () => {
-//   refresh();
-//   expect(!userService.isLoggedIn()).toBeTruthy();
-//   expect(!userService.login('username', 'password')).toBeTruthy();
-//   expect(userService.register('email', 'username', 'password')).toEqual({
-//     username: 'username', email: 'email', score: 0,
-//   });
-//   expect(userService.isLoggedIn()).toBeTruthy();
-//     expect(true).toBeTruthy();
-// });
-//
-// it('logout', () => {
-//   refresh();
-//   let user;
-//   userService.register('email', 'username', 'password').then(data => user = data).catch(data => user = data);
-//   expect(user).toEqual({
-//     username: 'username', email: 'email', score: 0,
-//   });
-//
-//   expect(userService.isLoggedIn()).toBeTruthy();
-//   expect(userService.logout()).toBeTruthy();
-//   expect(!userService.isLoggedIn()).toBeTruthy();
-//   expect(!userService.logout()).toBeTruthy();
-// });
+import userService from '../../public/services/userService';
+import User from '../../public/models/user';
+import Http from '../../public/modules/http';
+import {isUndefined} from 'util';
+
+let curUser: User | null = null;
+const registredUsers: User[] = [];
+
+function prepareFetchbody(body: User): User | null {
+  if (!body || !body.username || !body.password) {
+    return null;
+  }
+
+  body.score = 0;
+  delete body.password;
+
+  return body;
+}
+
+function refresh(): void {
+  registredUsers.length = 0;
+  curUser = null;
+}
+
+// Mock Http.Fetch
+Http.Fetch = function Fetch(method: string, path: string, body: any = undefined): Promise<Response | null> {
+  switch (`${method} ${path}`) {
+    case 'POST /user/signup':
+      body = prepareFetchbody(body);
+      if (curUser) {
+        return Promise.resolve(null);
+      }
+      registredUsers.push(body);
+      curUser = body;
+      return Promise.resolve(new Response(curUser));
+    case 'GET /user':
+      return Promise.resolve(new Response(curUser));
+    case 'GET /leaderboard':
+      return Promise.resolve().then(() => {
+        return new Response(
+          registredUsers.sort((a, b) => {
+            if (isUndefined(a.score) || isUndefined(b.score)) {
+              throw Error('score is undefined');
+            }
+            return a.score - b.score;
+          })
+        );
+      });
+    case 'POST /user/logout':
+      if (curUser) {
+        curUser = null;
+        return Promise.resolve(true as any);
+      }
+      return Promise.resolve(false as any);
+    case 'POST /user/signin':
+      body = prepareFetchbody(body);
+      if (curUser) {
+        return Promise.resolve(null);
+      }
+      curUser = curUser || registredUsers.find(el => el.username === body.username &&
+        el.password === body.password) || null;
+      return Promise.resolve(new Response(curUser));
+    default:
+      return Promise.reject(Error(`Origin API method ${method} ${path} calling is not permitted!`));
+  }
+};
+
+describe('UserService test', () => {
+  it('constructor', () => {
+    expect(userService && !userService.user).toBeTruthy();
+    expect(userService.users).toEqual([]);
+  });
+
+  it('register', () => {
+    refresh();
+    const actual = {username: 'username', email: 'email', score: 0};
+    let expected: User | null = null;
+    userService.register('email', 'username', 'password').then(data => expected = data);
+    expect(expected).toBe(actual);
+    expect(userService.users).toEqual([actual]);
+    expect(userService.user).toEqual(actual);
+    // expect(userService.fetch()).toBe(user);
+    expect(userService.isLoggedIn()).toBeTruthy();
+  });
+
+  it('login', () => {
+    refresh();
+    expect(!userService.isLoggedIn()).toBeTruthy();
+    expect(!userService.login('username', 'password')).toBeTruthy();
+    expect(userService.register('email', 'username', 'password')).toEqual({
+      username: 'username', email: 'email', score: 0,
+    });
+    expect(userService.isLoggedIn()).toBeTruthy();
+    expect(true).toBeTruthy();
+  });
+
+  it('logout', () => {
+    refresh();
+    let user: User | null = null;
+    userService.register('email', 'username', 'password')
+      .then(data => user = data);
+    expect(user).toEqual({
+      username: 'username', email: 'email', score: 0,
+    });
+
+    expect(userService.isLoggedIn()).toBeTruthy();
+    expect(userService.logout()).toBeTruthy();
+    expect(!userService.isLoggedIn()).toBeTruthy();
+    expect(!userService.logout()).toBeTruthy();
+  });
+});
---
+++ tests/unit/utils.test.ts
@@ -0,0 +1,26 @@
+import {throwIfNull, mapEventDirection} from '../../public/utils/utils';
+import {EVENT} from '../../public/utils/constants';
+
+describe('utils', () => {
+  it('throwIfNull', () => {
+    expect(() => {throwIfNull(null)}).toThrow('Variable must not be null');
+    let a : boolean | null = null;
+    expect(() => {throwIfNull(a)}).toThrow('Variable must not be null');
+    a = true;
+    expect(() => {
+      let b : boolean;
+      b = throwIfNull(a);
+      return b;
+    }).toBeTruthy();
+  });
+
+  it('mapEventDirection', () => {
+    expect(mapEventDirection(EVENT.DOWN)).toBe(180);
+    expect(mapEventDirection(EVENT.UP)).toBe(0);
+    expect(mapEventDirection(EVENT.LEFT)).toBe(270);
+    expect(mapEventDirection(EVENT.RIGHT)).toBe(90);
+    expect(mapEventDirection(EVENT.FIRE)).toBe(null);
+    expect(mapEventDirection(EVENT.TOWER)).toBe(null);
+    expect(mapEventDirection(EVENT.NO)).toBe(null);
+  });
+});
