3f3de8be7ca44c0ca1853e3b94a734fb7c1846c9
SpaceInvasionLab <spaceinvasionlab@yandex.ru>
2017-12-15 14:18:01 +0000

    http -> https and ws -> wss

--- public/utils/constants.ts
+++ public/utils/constants.ts
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://space-invasion.ru:8080/v1';
-const WEB_SOCKETS_BASE_URL = 'ws://space-invasion.ru:8080/game';
+const BASE_URL = 'https://space-invasion.ru:8080/v1';
+const WEB_SOCKETS_BASE_URL = 'wss://space-invasion.ru:8080/game';
 const MAX_EVENTS = 100;
 const RESPAWN_DAMAGE = 1;
 
